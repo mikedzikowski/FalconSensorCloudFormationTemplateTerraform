51d3f788dc150395e4160e63d22b41d5baa3659e
Mike Dzikowski <m_dzikowski@hotmail.com>
2026-01-08 11:06:42 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -59,3 +59,63 @@ variable "bottlerocket_ami_id" {
   description = "Bottlerocket AMI ID for ECS"
   type        = string
 }
+
+variable "asg_desired_capacity" {
+  description = "Desired number of instances in ASG"
+  type        = number
+  default     = 2
+}
+
+variable "asg_max_size" {
+  description = "Maximum number of instances in ASG"
+  type        = number
+  default     = 4
+}
+
+variable "asg_min_size" {
+  description = "Minimum number of instances in ASG"
+  type        = number
+  default     = 1
+}
+
+variable "tags" {
+  description = "A map of tags to add to all resources"
+  type        = map(string)
+  default     = {}
+}
+
+variable "enable_monitoring" {
+  description = "Enable detailed monitoring for EC2 instances"
+  type        = bool
+  default     = true
+}
+
+variable "root_volume_size" {
+  description = "Size of the root volume in GB"
+  type        = number
+  default     = 30
+}
+
+variable "root_volume_type" {
+  description = "Type of the root volume (gp2, gp3, io1, etc.)"
+  type        = string
+  default     = "gp3"
+}
+
+variable "enable_termination_protection" {
+  description = "Enable termination protection for EC2 instances"
+  type        = bool
+  default     = false
+}
+
+variable "subnet_count" {
+  description = "Number of subnets to create"
+  type        = number
+  default     = 2
+}
+
+variable "enable_nat_gateway" {
+  description = "Enable NAT Gateway for private subnets"
+  type        = bool
+  default     = false
+}
