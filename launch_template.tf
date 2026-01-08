307bdb8f8ac7f185ee60c2d3e72138711f73a8b7
Mike Dzikowski <m_dzikowski@hotmail.com>
2026-01-08 10:56:26 -0500

    Create launch_template.tf

---
+++ launch_template.tf
@@ -0,0 +1,53 @@
+# Create launch template
+resource "aws_launch_template" "ecs" {
+  name_prefix   = "${var.environment}-ecs-template"
+  image_id      = data.aws_ami.bottlerocket_ami.id
+  instance_type = var.instance_type
+
+  user_data = base64encode(<<-EOF
+    [settings.ecs]
+    cluster = "${aws_ecs_cluster.main.name}"
+    [settings.host-containers.admin]
+    enabled = true
+    [settings.host-containers.control]
+    enabled = true
+    EOF
+  )
+
+  network_interfaces {
+    associate_public_ip_address = true
+    security_groups            = [aws_security_group.ecs_instances.id]
+  }
+
+  iam_instance_profile {
+    name = aws_iam_instance_profile.ecs_instance_profile.name
+  }
+
+  tag_specifications {
+    resource_type = "instance"
+    tags = {
+      Name = "${var.environment}-ecs-instance"
+    }
+  }
+}
+
+# Create Auto Scaling Group
+resource "aws_autoscaling_group" "ecs" {
+  name                = "${var.environment}-ecs-asg"
+  desired_capacity    = var.asg_desired_capacity
+  max_size           = var.asg_max_size
+  min_size           = var.asg_min_size
+  target_group_arns  = []
+  vpc_zone_identifier = aws_subnet.public[*].id
+
+  launch_template {
+    id      = aws_launch_template.ecs.id
+    version = "$Latest"
+  }
+
+  tag {
+    key                 = "AmazonECSManaged"
+    value              = "true"
+    propagate_at_launch = true
+  }
+}
