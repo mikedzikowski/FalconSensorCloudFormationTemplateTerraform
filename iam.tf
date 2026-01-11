217c8a851af60a2537d11fb84da8aebc14324982
Michael Dzikowski <m_dzikowski@hotmail.com>
2026-01-11 12:14:12 -0500

    Reapply "Create iam.tf"
    This reverts commit 799418fd49bcbc538edddd980ee95377910327f8.

---
+++ iam.tf
@@ -0,0 +1,31 @@
+# Create IAM role for ECS instances
+resource "aws_iam_role" "ecs_instance_role" {
+  name = "${var.environment}-ecs-instance-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+
+  tags = var.tags
+}
+
+# Attach ECS instance policy
+resource "aws_iam_role_policy_attachment" "ecs_instance_role_policy" {
+  role       = aws_iam_role.ecs_instance_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
+}
+
+# Create instance profile
+resource "aws_iam_instance_profile" "ecs_instance_profile" {
+  name = "${var.environment}-ecs-instance-profile"
+  role = aws_iam_role.ecs_instance_role.name
+}
