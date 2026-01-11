a640e77eb7cecf8097553125b95b7c5553a266cf
Mike Dzikowski <m_dzikowski@hotmail.com>
2026-01-11 10:19:45 -0500

    Update launch_template.tf

--- launch_template.tf
+++ launch_template.tf
@@ -1,16 +1,14 @@
-# Create launch template
+# Launch template and ASG - Only created for new cluster
 resource "aws_launch_template" "ecs" {
+  count = local.create_new_cluster ? 1 : 0
+
   name_prefix   = "${var.environment}-ecs-template"
   image_id      = data.aws_ami.bottlerocket_ami.id
   instance_type = var.instance_type
 
-  monitoring {
-    enabled = var.enable_monitoring
-  }
-
   user_data = base64encode(<<-EOF
     [settings.ecs]
-    cluster = "${aws_ecs_cluster.main.name}"
+    cluster = "${local.cluster_name}"
     [settings.host-containers.admin]
     enabled = true
     [settings.host-containers.control]
@@ -19,21 +17,12 @@ resource "aws_launch_template" "ecs" {
   )
 
   network_interfaces {
-    associate_public_ip_address = !var.enable_nat_gateway
+    associate_public_ip_address = true
     security_groups            = [aws_security_group.ecs_instances.id]
   }
 
   iam_instance_profile {
     name = aws_iam_instance_profile.ecs_instance_profile.name
-  }
-
-  block_device_mappings {
-    device_name = "/dev/xvda"
-    ebs {
-      volume_size = var.root_volume_size
-      volume_type = var.root_volume_type
-      encrypted   = true
-    }
   }
 
   tag_specifications {
@@ -45,37 +34,35 @@ resource "aws_launch_template" "ecs" {
       }
     )
   }
-
-  tags = var.tags
 }
 
-# Create Auto Scaling Group
 resource "aws_autoscaling_group" "ecs" {
+  count = local.create_new_cluster ? 1 : 0
+
   name                = "${var.environment}-ecs-asg"
   desired_capacity    = var.asg_desired_capacity
   max_size           = var.asg_max_size
   min_size           = var.asg_min_size
   target_group_arns  = []
-  vpc_zone_identifier = aws_subnet.public[*].id
+  vpc_zone_identifier = local.subnet_ids
 
   launch_template {
-    id      = aws_launch_template.ecs.id
+    id      = aws_launch_template.ecs[0].id
     version = "$Latest"
   }
 
+  tag {
+    key                 = "AmazonECSManaged"
+    value              = "true"
+    propagate_at_launch = true
+  }
+
   dynamic "tag" {
-    for_each = merge(
-      var.tags,
-      {
-        AmazonECSManaged = "true"
-      }
-    )
+    for_each = var.tags
     content {
       key                 = tag.key
       value              = tag.value
       propagate_at_launch = true
     }
   }
-
-  protect_from_scale_in = var.enable_termination_protection
 }
